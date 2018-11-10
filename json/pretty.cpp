3a3cfb477f3e3de7c74e47044af8df5a69fca7fc
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-11-10 13:53:39 +0300

    update

--- json/pretty.cpp
+++ json/pretty.cpp
@@ -8,6 +8,16 @@ public:
 
     void pretty()
     {
+        Token *token;
+
+        do {
+            token = this->stream->get_next_token();
+            if (token != NULL) {
+                cout << "Column: " << token->getColumn() << endl;
+                cout << "Line: " << token->getLine() << endl;
+            }
+        } while (token != NULL);
+
         return;
     }
 private:
--- json/tokens/token.cpp
+++ json/tokens/token.cpp
@@ -7,6 +7,16 @@ public:
         this->column = column;
     }
 
+    int getLine()
+    {
+        return this->line;
+    }
+
+    int getColumn()
+    {
+        return this->column;
+    }
+
 protected:
     int line;
     int column;
