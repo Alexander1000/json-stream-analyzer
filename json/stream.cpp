b73552f323003f3b739976de9c6f9bdd5db4d0be
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-11-10 11:43:43 +0300

    prepare json streamer

--- json/stream.cpp
+++ json/stream.cpp
@@ -4,8 +4,17 @@ public:
     Stream(IOReader* reader)
     {
         this->reader = reader;
+
+        this->currentBuffer = new char[STREAM_BUFFER_SIZE];
+        this->forwardBuffer = new char[STREAM_BUFFER_SIZE];
+
+        memset(this->currentBuffer, 0, STREAM_BUFFER_SIZE * sizeof(char));
+        memset(this->forwardBuffer, 0, STREAM_BUFFER_SIZE * sizeof(char));
     }
 
 private:
     IOReader* reader;
+
+    char* currentBuffer;
+    char* forwardBuffer;
 };
--- json/stream.h
+++ json/stream.h
@@ -1,2 +1,4 @@
 #include "stream.cpp"
 #include "pretty.cpp"
+
+STREAM_BUFFER_SIZE = 4096;
