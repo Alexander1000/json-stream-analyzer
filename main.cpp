7d689ac5528ab254622e38d62202152edce29ef7
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-11-10 11:32:41 +0300

    add command line package

--- main.cpp
+++ main.cpp
@@ -1,4 +1,67 @@
-int main()
+#include <iostream>
+#include "cl.cpp"
+#include <stdio.h>
+#include <sys/stat.h>
+
+using namespace std;
+
+#define ERROR_MISSING_INPUT -1
+#define ERROR_STAT_INFO -2
+#define ERROR_STAT_INVALID_MODE -3
+#define ERROR_NOT_IMPLEMENT -4
+
+#define ERROR_INVALID -255
+
+void help()
 {
+    cout << "Help message for json-stream-analyzer:" << endl;
+    cout << "-i [--input] - file for analyze" << endl;
+    cout << "-h [--help] - this help message" << endl;
+}
+
+int main(int argc, char* argv[])
+{
+    CommandLine commandLine(argc, argv);
+
+    if (commandLine.is("-h") || commandLine.is("--help")) {
+        help();
+        return 0;
+    }
+
+    if (!commandLine.is("-i") && !commandLine.is("--input")) {
+        help();
+        return ERROR_MISSING_INPUT;
+    }
+
+    char* inputPath = NULL;
+
+    if (commandLine.is("-i")) {
+        inputPath = commandLine.get("-i");
+    } else if (commandLine.is("--input")) {
+        inputPath = commandLine.get("--input");
+    }
+
+    if (inputPath == NULL) {
+        cout << "Missing parameter value for -i [--input]" << endl;
+        return ERROR_MISSING_INPUT;
+    }
+
+    struct stat s;
+    int checkStat = stat(inputPath, &s);
+
+    if (checkStat != 0) {
+        cout << "Undefined errors: " << checkStat << endl;
+        return ERROR_STAT_INFO;
+    }
+
+    if (s.st_mode & S_IFDIR) {
+        cout << "is a directory" << endl;
+        return ERROR_NOT_IMPLEMENT;
+    } else if(s.st_mode & S_IFREG) {
+        cout << "bingo" << endl;
+    } else {
+        return ERROR_STAT_INVALID_MODE;
+    }
+
     return 0;
 }
