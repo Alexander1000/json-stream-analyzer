6d88cfe8ae2f0df83a0cb586b89cfd5f69f84052
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-11-10 13:38:24 +0300

    update

--- json/stream.h
+++ json/stream.h
@@ -1,8 +1,8 @@
-#include "stream.cpp"
-#include "pretty.cpp"
-
 #define STREAM_BUFFER_SIZE = 4096;
 
 #define JSON_LEXER_PLAIN_MODE 0
 #define JSON_LEXER_TEXT_MODE 1
 #define JSON_LEXER_DIGIT_MODE 2
+
+#include "stream.cpp"
+#include "pretty.cpp"
