95b2f8a99537d34a8c241730b6a2aed6da381371
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-11-10 12:02:27 +0300

    update analyzer json

--- json/stream.cpp
+++ json/stream.cpp
@@ -10,6 +10,8 @@ public:
 
         memset(this->currentBuffer, 0, STREAM_BUFFER_SIZE * sizeof(char));
         memset(this->forwardBuffer, 0, STREAM_BUFFER_SIZE * sizeof(char));
+
+        this->currentPosition = 0;
     }
 
     ~Stream()
@@ -18,9 +20,23 @@ public:
         delete this->forwardBuffer;
     }
 
+    void get_next_token()
+    {}
+
 private:
     IOReader* reader;
 
     char* currentBuffer;
     char* forwardBuffer;
+
+    // текущая позиция для чтения
+    int currentPosition;
+
+    // позиция начала текущего блока
+    int posCurrent;
+    // позиция начала следующего блока
+    int posForward;
+
+    // признак конца потока
+    bool eof;
 };
