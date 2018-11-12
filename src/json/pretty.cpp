6a1117481c1cb63c20ca6498b9f5ad546dc0a599
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-11-12 08:33:26 +0300

    update

--- src/json/pretty.cpp
+++ src/json/pretty.cpp
@@ -10,19 +10,76 @@ public:
     {
         Token *token;
         IOReader* reader;
+        int offset = 0;
+
+        bool needOffset = false;
 
         do {
             token = this->stream->get_next_token();
-            if (token != NULL) {
-                reader = token->getReader();
-                char* content = new char[512];
-                reader->read(content, 512);
-                cout << content << endl;
+            if (token == NULL) {
+                break;
             }
-        } while (token != NULL);
+
+            if (needOffset) {
+                this->print_offset(offset);
+                needOffset = false;
+            }
+
+            switch (token->getType()) {
+                case TOKEN_TYPE_BRACES_OPEN:
+                    cout << "{" << endl;
+                    offset++;
+                    needOffset = true;
+                    break;
+                case TOKEN_TYPE_BRACES_CLOSE:
+                    offset--;
+                    cout << "}" << endl;
+                    break;
+                case TOKEN_TYPE_QUOTES:
+                    cout << '"';
+                    needOffset = false;
+                    break;
+                case TOKEN_TYPE_ARRAY_OPEN:
+                    cout << "[" << endl;
+                    needOffset = true;
+                    offset++;
+                    break;
+                case TOKEN_TYPE_ARRAY_CLOSE:
+                    offset--;
+                    cout << endl;
+                    this->print_offset(offset);
+                    cout << "]" << endl;
+                    needOffset = true;
+                    break;
+                case TOKEN_TYPE_COLON:
+                    cout << ":";
+                    break;
+                case TOKEN_TYPE_COMMA:
+                    cout << "," << endl;
+                    needOffset = true;
+                    break;
+                case TOKEN_TYPE_TEXT:
+                    reader = token->getReader();
+                    char* text = new char[512];
+                    reader->read(text, 512);
+                    cout << text;
+                    break;
+            }
+        } while (true);
 
         return;
     }
 private:
     Stream *stream;
+
+    void print_offset(int offset)
+    {
+        if (offset == 0) {
+            return;
+        }
+
+        char *space = new char[offset * 4];
+        memset(space, 0x20, offset * 4 * sizeof(char));
+        cout << space;
+    }
 };
