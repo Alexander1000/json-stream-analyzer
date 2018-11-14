8fe7ab2fab5146c2a5f8b3c3b1312ca45bcc595b
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-11-14 08:07:37 +0300

    update

--- src/json/object/value.cpp
+++ src/json/object/value.cpp
@@ -1,7 +1,22 @@
 class Value
 {
 public:
-    Value()
+    Value(int type, void* data)
     {
+        this->type = type;
+        this->data = data;
     }
+
+    int getType()
+    {
+        return this->type;
+    }
+
+    void* getData()
+    {
+        return this->data;
+    }
+private:
+    int type;
+    void* data;
 };
