f1f75abd7ad97bd5aab497044f1b96794115c0ed
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-11-17 10:04:43 +0300

    skip first token

--- src/json/decoder.cpp
+++ src/json/decoder.cpp
@@ -9,6 +9,20 @@ public:
     Object* decode()
     {
         Object* obj = new Object();
+        Token *token;
+
+        // begin // временное решение, json может начаться массивом
+        token = this->stream->get_next_token();
+        if (token->getType() != TOKEN_TYPE_BRACES_OPEN) {
+            return NULL;
+        }
+        // end //
+
+        token = this->stream->get_next_token();
+        while(token != NULL) {
+            token = this->stream->get_next_token();
+        }
+
         return obj;
     }
 
