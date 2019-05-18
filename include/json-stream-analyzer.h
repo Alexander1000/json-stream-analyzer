5a007e167d0a501f28010055ef0dc4715bf5edb4
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-05-18 13:41:08 +0300

    update

--- include/json-stream-analyzer.h
+++ include/json-stream-analyzer.h
@@ -1,4 +1,8 @@
-// #include <json-stream-analyzer/io_buffer.h>
+#ifndef JSON_STREAM_ANALYZER_INCLUDED
+#define JSON_STREAM_ANALYZER_INCLUDED
+
 #include <json-stream-analyzer/stream.h>
 #include <json-stream-analyzer/token.h>
 #include <json-stream-analyzer/element.h>
+
+#endif /* JSON_STREAM_ANALYZER_INCLUDED */
