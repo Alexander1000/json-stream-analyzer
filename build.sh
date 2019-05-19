36dbe0b82cd29b821733f9390b970d62a91feb95
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-05-19 14:42:44 +0300

    cmake

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# set -xe
+
 rm -rf CMakeFiles
 rm -f CMakeCache.txt
+rm -f Makefile
+rm -f libjson-stream-analyzer*
 
 cmake .
 make
 make install
 
 cd cli
+
+echo "Compile cli application"
 
 rm -rf CMakeFiles
 rm -f CMakeCache.txt
