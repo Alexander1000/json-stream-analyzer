58f092c6408d97703a2023678a05d8df23d63356
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-05-19 14:47:33 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -2,21 +2,58 @@
 
 # set -xe
 
+echo "#--------------------------#"
+echo "#      BUILD LIBRARY       #"
+echo "#--------------------------#"
+
+echo "+++++++++++++++++"
+echo "++ CLEAR CACHE ++"
+echo "+++++++++++++++++"
+
 rm -rf CMakeFiles
 rm -f CMakeCache.txt
 rm -f Makefile
 rm -f libjson-stream-analyzer*
 
+echo "+++++++++++++++++++"
+echo "++     CMAKE     ++"
+echo "+++++++++++++++++++"
+
 cmake .
+
+echo "++++++++++++++++++"
+echo "++     MAKE     ++"
+echo "++++++++++++++++++"
+
 make
+
+echo "+++++++++++++++++++++"
+echo "++     INSTALL     ++"
+echo "+++++++++++++++++++++"
+
 make install
 
 cd cli
 
-echo "Compile cli application"
+echo "#------------------------------#"
+echo "#      BUILD APPLICATION       #"
+echo "#------------------------------#"
+
+echo "+++++++++++++++++"
+echo "++ CLEAR CACHE ++"
+echo "+++++++++++++++++"
 
 rm -rf CMakeFiles
 rm -f CMakeCache.txt
 
+echo "+++++++++++++++++++"
+echo "++     CMAKE     ++"
+echo "+++++++++++++++++++"
+
 cmake .
+
+echo "++++++++++++++++++"
+echo "++     MAKE     ++"
+echo "++++++++++++++++++"
+
 make
