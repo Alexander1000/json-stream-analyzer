39835f3e02361106ac919d08d6d6ba10145737a0
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-05-19 15:15:07 +0300

    update nested namespace

--- src/buffer/io_buffer.cpp
+++ src/buffer/io_buffer.cpp
@@ -1,36 +1,32 @@
-namespace JsonStreamAnalyzer {
+namespace JsonStreamAnalyzer::Buffer {
 
-    namespace Buffer {
+    class IOWriter {
+    public:
+        /**
+         * Запись в IO length байт
+         * @param char*
+         * @param length
+         * @return int количество записанных данных
+         */
+        virtual int write(char *buffer, int length) = 0;
+    };
 
-        class IOWriter {
-        public:
-            /**
-             * Запись в IO length байт
-             * @param char*
-             * @param length
-             * @return int количество записанных данных
-             */
-            virtual int write(char *buffer, int length) = 0;
-        };
-
-        class IOReader {
-        public:
-            /**
-             * Чтение из IO length байт
-             * @param char*
-             * @param length
-             * @return int количество прочитанных данных
-             */
-            virtual int read(char *buffer, int length) = 0;
-        };
+    class IOReader {
+    public:
+        /**
+         * Чтение из IO length байт
+         * @param char*
+         * @param length
+         * @return int количество прочитанных данных
+         */
+        virtual int read(char *buffer, int length) = 0;
+    };
 
 
-        class IOBuffer : public IOReader, public IOWriter {
-        public:
-            IOBuffer() : IOReader(), IOWriter() {
-            }
-        };
+    class IOBuffer : public IOReader, public IOWriter {
+    public:
+        IOBuffer() : IOReader(), IOWriter() {
+        }
+    };
 
-    } // Buffer
-
-} // JsonStreamAnalyzer
+} // JsonStreamAnalyzer::Buffer
