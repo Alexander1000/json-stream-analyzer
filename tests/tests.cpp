5ae35ccc896b692fc5b0588c3a8feabb6ed56c60
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-06-03 08:58:20 +0300

    add asserts

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,10 +1,25 @@
+#include <iostream>
+
 #include <json-stream-analyzer.h>
 #include <io-buffer.h>
+
+class AssertElementTypeException
+{};
+
+void assertType(JsonStreamAnalyzer::Element* element, int expectedType) {
+    if (element->getType() != expectedType) {
+        std::cout << "Expected element type: " << expectedType << "; but given: " << element->getType() << std::endl;
+        throw new AssertElementTypeException;
+    }
+}
 
 int main(int argc, char** argv) {
     IOBuffer::IOFileReader file_buffer("../fixtures/001-simple-example.json");
     JsonStreamAnalyzer::Stream json_stream(&file_buffer);
     JsonStreamAnalyzer::Decoder decoder(&json_stream);
     JsonStreamAnalyzer::Element* object = decoder.decode();
+
+    assertType(object, ELEMENT_TYPE_OBJECT);
+
     return 0;
 }
